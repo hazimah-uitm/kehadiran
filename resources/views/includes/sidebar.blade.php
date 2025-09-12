fcb6244ed812bf8b9053b8d1f3d4769e4f2979e4
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-09-12 09:24:14 +0800

    updated img

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -3,8 +3,8 @@
         <img src="{{ asset('public/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
     </div>
     <div>
-        <h4 class="logo-text">MAIN</h4>
-        <h6 class="logo-subtitle">Template System</h6>
+        <h4 class="logo-text">SISTEM KEHADIRAN</h4>
+        {{-- <h6 class="logo-subtitle">Template System</h6> --}}
     </div>
     <div class="toggle-icon ms-auto" id="toggle-icon"><i class='bx bx-arrow-to-left'></i></div>
 </div>
