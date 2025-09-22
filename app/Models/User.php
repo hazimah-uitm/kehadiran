baefc5354a32b0ae55212256229a9a69052fd71f
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-09-23 06:47:13 +0800

    updated user

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Position::class);
     }
+
+    public function ptj()
+    {
+        return $this->belongsTo(Ptj::class);
+    }
 }
